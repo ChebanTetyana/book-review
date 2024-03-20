9d12de35616780de91a9c8298c15b310afaf7d5e
ChebanTetyana <kapitoskay1302@gmail.com>
2024-03-20 12:59:24 +0200

    Create routs resource books and books review

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
+
+Route::resource('books', \App\Http\Controllers\BookController::class)
+    ->only(['index', 'show']);
+
+Route::resource('books.reviews', \App\Http\Controllers\ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create', 'store']);
+
